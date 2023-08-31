3c0753776366d4262e9ea61fb53a37d2580f2d1f
luizAFS23 <luizafs.1500@gmail.com>
2023-08-31 16:34:29 -0300

    changes

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
+const prompt = require("prompt-sync")();
+
 function getComputerChoice(){
     let choice = ['rock', 'paper', 'scissors'];
     let randomchoice = Math.floor(Math.random() * (choice.length - 1 - 0 + 1)) + 0;
     return choice[randomchoice];
 }
 
-const userChoice = prompt("Choose between rock, paper and scissors (Exactly like this!");
-console.log(getComputerChoice());
+function youLose(){
+    return "You lose.";
+}
+
+function youWin(){
+    return "You win!";
+}
+
+function playRound(userChoice, computerChoice){
+    switch(userChoice){
+        case userChoice == 'rock' && computerChoice == 'scissors':
+            console.log(youWin());
+            break;
+        case userChoice == 'scissors' && computerChoice == 'rock':
+            console.log(youLose());
+            break;
+        case userChoice == 'paper' && computerChoice == 'rock':
+            console.log(youWin());
+            break;
+        case userChoice == 'paper' && computerChoice == 'scissors':
+            console.log(youLose());
+            break;
+        default:
+            console.log("Invalid choice.");
+            break;
+    }
+}
+
+const userChoice = prompt("Choose between rock, paper and scissors (Exactly like this!) ");
+const computerChoice = getComputerChoice();
+
+console.log(playRound(userChoice, computerChoice));
